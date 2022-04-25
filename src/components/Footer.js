deb4b0753db179e5488473ed39b2d7436e521272
Devon Faria <devonfaria@gmail.com>
2022-04-25 16:53:21 -0400

    created html exports for header and footer with inline styles applied

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,21 @@
 import React from 'react';
 
+const styles = {
+  footer: {
+    backgroundColor: '#6289de',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+};
+
 export default function Footer() {
-  return console.log('Footer plugged in');
+  console.log('Footer plugged in');
+  return (
+    <div>
+      <footer style={styles.footer}>
+        <h5>Devon Faria 2022</h5>
+      </footer>
+    </div>
+  )
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,30 @@
 import React from 'react';
 
+const styles = {
+  header: {
+    backgroundColor: '#6472ff',
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  links: {
+    color: 'black',
+    margin: '1rem'
+  },
+
+};
+
 export default function Header() {
-  return console.log('Header plugged in');
+  console.log('Header plugged in');
+  return (
+    <header style={styles.header}>
+      <h1>Devon Faria</h1>
+      <nav>
+        <a href='/' style={styles.links}>About Me</a>
+        <a href='/' style={styles.links}>Portfolio</a>
+        <a href='/' style={styles.links}>Contact</a>
+        <a href='/' style={styles.links}>Resume</a>
+      </nav>
+    </header>
+  )
 };
