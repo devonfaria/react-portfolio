d1f34f83dc7889ad1d462fb1682c609f76c585a6
Devon Faria <devonfaria@gmail.com>
2022-04-28 17:13:25 -0400

    removed React errors and final commit

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -6,7 +6,7 @@ export default function AboutMe() {
     <div className='container mt-5 mb-5'>
       <h1 id='page-title' className='mt-4 mb-4'>About Me</h1>
       <div className='container d-flex'>
-        <img className='mr-4 mt-4 mb-4' id='photo' src='https://res.cloudinary.com/dfamiaufc/image/upload/ar_1:1,b_rgb:ffffff,bo_0px_solid_rgb:ffffff,c_fill,g_auto,r_max,w_300/v1651018512/Portfolio/Devon-Faria-Headshot_qgmho4.jpg'></img>
+        <img className='mr-4 mt-4 mb-4' id='photo' src='https://res.cloudinary.com/dfamiaufc/image/upload/ar_1:1,b_rgb:ffffff,bo_0px_solid_rgb:ffffff,c_fill,g_auto,r_max,w_300/v1651018512/Portfolio/Devon-Faria-Headshot_qgmho4.jpg' alt='devon faria'></img>
         <p className='mt-4'>I am a 30 year old professional living in Asheville, NC currently looking for opportunities for junior    developers. With over a decade of experience in customer service in both real estate and non-profit, I offer unique perspectives to design, accesibility, and the customer experience.
           <br></br>
           <br></br>
--- src/components/pages/ProjectCard.js
+++ src/components/pages/ProjectCard.js
@@ -4,11 +4,11 @@ import React from 'react'
 const ProjectCard = (props) => {
   return (
     <div key={props.id} className="card col-lg-6 col-md-12 shadow p-3 mb-5 rounded">
-      <a href={props.link} className='link' target="_blank" rel='noreferrer noopener'>
-        <img className='card-img-top' src={props.imgLink}></img>
+      <a href={props.link} target="_blank" rel='noreferrer noopener'>
+        <img className='card-img-top' src={props.imgLink} alt='project webpage'></img>
       </a>
       <h3 className="project-header mb-4">
-        <a href={props.link} className='link' target="_blank" rel='noreferrer noopener'>{props.title}</a>
+        <a href={props.link} id='link' target="_blank" rel='noreferrer noopener'>{props.title}</a>
       </h3>
       <div className="card-body">
         <p>{props.description}</p>
